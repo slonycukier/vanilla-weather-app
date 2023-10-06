53f586a4e444ed509ac31edbe8df452e175b500f
slonycukier <91665948+slonycukier@users.noreply.github.com>
2023-10-06 15:33:25 +0200

    Applied some real-life data - temp, hum and wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".value");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#hum");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "a867e25f2d83db579421a57fd8e937ec";
+let cityName = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
